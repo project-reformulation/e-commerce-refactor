b90c5a0276ff5593d8bf0f34c3b17f8d6bea3c0b
Souhaiellahmed <144438520+Souhaiellahmed@users.noreply.github.com>
2024-02-23 09:03:24 +0100

    done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,6 @@
-
-
-export default function Home() {
-  return (
-    <main >
-     
 import Image from "next/image";
 import Link from "next/link";
+
 
 export default function Home() {
   return (
