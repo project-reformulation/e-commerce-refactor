837d0bd6aff434e566a4df693498fabf866b455f
boukhaddejatas <boukhaddejatas@gmail.com>
2024-02-25 20:36:48 +0100

    ...

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home () {
   return (
     <main>
       <Link href="/SignUp"></Link>
